e7630c03f24fb354aa1c20ad24cbcb932f7b0b0e
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-20 20:34:46 -0300

    TasksService injection on TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // Injetando TasksService no controller
+  constructor(private tasksService: TasksService) {}
+}
