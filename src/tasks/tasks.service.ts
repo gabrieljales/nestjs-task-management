6c0a61a4ed7424ee4f78c9a8590fdfa4d174125f
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-20 22:49:37 -0300

    Add task type

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = []; // Somente esse serviço tem acesso direto a esse array
+  private tasks: Task[] = []; // Somente esse serviço tem acesso direto a esse array
 
   // lembrete: Não é necessário definir o método como public, o TS já faz isso quando não definimos
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
