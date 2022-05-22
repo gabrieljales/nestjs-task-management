842008a5844f76d6dda3ded093648b2ae32ae650
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-21 22:50:11 -0300

    Validation: Validating task filtering and search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class getTasksFilterDto {
+  @IsOptional() // TS não existe em tempo de execução. Aqui garantimos que o campo é opcional na validação
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
 
