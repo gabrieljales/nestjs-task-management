25b9632c1d9160d091e6c4d211c255beec54df8a
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-22 16:09:07 -0300

    Database connection (for now, without env variables)

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: 'password',
       database: 'task-management',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // Não recomendado em produção
     }),
   ],
 })
