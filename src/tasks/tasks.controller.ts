cb7dd77f9a5c7a5128bc5f3773b1129a03bf623b
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-21 22:40:33 -0300

    Validation: Update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,8 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { Task, TaskStatus } from './task.model';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks') // Rota /tasks
@@ -27,8 +28,6 @@ export class TasksController {
     } else {
       return this.tasksService.getAllTasks();
     }
-
-    return this.tasksService.getAllTasks();
   }
 
   @Get('/:id') // Parâmetro de url
@@ -49,8 +48,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
