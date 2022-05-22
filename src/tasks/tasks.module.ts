48ecfe6ff23e88ad8a26e985b3747efebfdae2f0
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-22 17:16:43 -0300

    Task Module: TasksRepository added (Dep. Injec.)
    This line allow us to dependecy inject the TasksRepository whenever we need

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
+import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository])], // Repositório disponível em qualquer lugar do módulo (I.D)
   controllers: [TasksController],
   providers: [TasksService],
 })
