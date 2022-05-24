8739e01f03b76382bbba35a9885cebee183c8839
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-24 16:34:41 -0300

    Regex explanation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
+
+// Regex:
+// Conter no mínimo uma letra maiúscula
+// Conter no mínimo uma letra minúscula
+// Conter no mínimo um número ou caractere especial
+// Sem validação de tamanho min. e max
