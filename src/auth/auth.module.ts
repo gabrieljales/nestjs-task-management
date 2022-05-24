9cec5c9aef87baa5684dfab9fdd4aa891d52bb5c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-24 20:22:16 -0300

    Feature: JWT validation strategy implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,7 +18,8 @@ import { JwtModule } from '@nestjs/jwt';
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule], // Qualquer outro módulo pode aproveitar-se desse mecanismo de auth ao import o auth.module
 })
 export class AuthModule {}
