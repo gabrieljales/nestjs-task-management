8e313cfbfe78a0113403eee6a6a5c361167cba02
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-24 15:04:45 -0300

    Custom repositories: getTasks (with filter) and createTask

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,5 +1,43 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { getTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 
 @EntityRepository(Task)
-export class TasksRepository extends Repository<Task> {}
+export class TasksRepository extends Repository<Task> {
+  async getTasks(filterDto: getTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+
+    // Argumento 'task': como vamos nos referir a entidade Task na query
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      // ':status': Variável da query, que depois recebe um novo valor em { status }
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` }, // %: Permite buscar por partes independentes da palavra
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+
+    await this.save(task);
+
+    return task;
+  }
+}
