9e93d7066fbeb6660afcb30053added2208d2628
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-26 17:17:58 -0300

    Comment: Explaining the toPlainOnly property

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -18,6 +18,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
-  @Exclude({ toPlainOnly: true })
+  @Exclude({ toPlainOnly: true }) // Sempre que esse objeto for fornecido em texto simples(json), a propriedade user será excluída
   user: User;
 }
