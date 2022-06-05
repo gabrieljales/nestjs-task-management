49d102cf32fcbb2579d49f34234cee9eea45e0ea
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-06-04 22:10:45 -0300

    Documentation: using swagger config in app

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { swagger } from './doc/swagger.doc';
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // Habilitar os Validations Pipes
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  swagger(app);
 
   const port = process.env.PORT;
   await app.listen(port);
